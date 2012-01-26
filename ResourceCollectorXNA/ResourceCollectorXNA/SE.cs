742a6d1e94e6472421def4c5458331c53f428db9
lojkofob <lojkofob@github.com>
2012-01-26 16:17:34 +0300

    scripts"

---
+++ ResourceCollectorXNA/ResourceCollectorXNA/SE.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Reflection;
+using IronPython;
+using IronPython.Hosting;
+using IronPython.Runtime;
+using IronPython.Modules;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+using System.Xml;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ResourceCollectorXNA
+{
+    //Аттрибуты можно получить во время рантайм выполнения скрипта. Можно запихать сюда инфу для описания метода и прочее
+    public class Comment : System.Attribute    {        public string comment;      public Comment(string comment)      {    this.comment = comment; }    }
+
+    // Dictionary<string,dynamic> - using for UserData such as Config и т.п.
+    public abstract class MyPythonEngine : Dictionary<string,dynamic>
+    {
+        public string file_name = "";
+        public SCRIPTS scripts;
+        public ScriptEngine scriptengine;
+        public ScriptScope scriptscope;
+        public virtual void SetVariables() { }
+        
+        public MyPythonEngine(string path)
+            : base()
+        {
+            file_name = path;
+            scripts = new SCRIPTS(path);
+            scriptengine = IronPython.Hosting.Python.CreateEngine();
+            scriptscope = scriptengine.CreateScope();
+            scriptscope.SetVariable("result", "");
+        }
+
+        /// <summary>
+        /// Please, do not use while game is running and user is killing monsters.
+        /// It's extra slow. Do not use in scripts!!!
+        /// </summary>
+        public Dictionary<string, dynamic> Variables
+        {
+            get
+            {
+                List<string> names = new List<string>(scriptscope.GetVariableNames());
+                Dictionary<string, dynamic> vars = new Dictionary<string, dynamic>(names.Count);
+                foreach (string name in names)
+                    try
+                    {
+                        vars[name] = scriptscope.GetVariable(name);
+                    }
+                    catch
+                    {
+                        vars[name] = "ERROR on Getvariable";
+                    }
+                return vars;
+            }
+        }
+        public Exception LastException;
+        public void Execute(string str)
+        {
+            LastException = null;
+            if (str != scripts.Empty.data)
+            {
+                try
+                {
+                    scriptengine.Execute(str, scriptscope);
+                }
+                catch (Exception ee)
+                {
+                    LastException = ee;
+                   ConsoleWindow.TraceMessage(ee.Message);
+                }
+               /* try { return scriptscope.GetVariable<string>("result"); }       catch { }
+                try { return scriptscope.GetVariable<object>("result"); }       catch { }*/
+            }
+           // return "";
+        }
+
+        public virtual void ExScript(string script_name)
+        {
+               scriptscope.SetVariable("counter", scripts[script_name].counter++);
+               Execute(scripts[script_name].data);
+               if (LastException!=null)
+                 ConsoleWindow.TraceMessage("Executing Script " + script_name + " : " + LastException.Message);
+            //return scripts[script_name].Execute();
+        }
+
+        public virtual void FillByVariables(string name_pattern)
+        {
+            Regex regex = new Regex(name_pattern);
+            Dictionary<string, dynamic> vars = Variables;
+            foreach (string name in vars.Keys)
+            {
+                if (regex.IsMatch(name))
+                Add(name, vars[name]);
+            }
+        }
+
+
+    }
+
+    public class SE : MyPythonEngine
+    {
+        public SE(string path) : base(path) { }
+        private static SE _instance;
+        public static SE Instance {
+            get {
+                if (_instance == null)
+                    _instance = new SE("Scripts");
+            return _instance;
+            }
+        }
+    }
+
+
+
+    public class SCRIPTS : AutoLoadingContent
+    {
+        public void SaveAll()
+        { foreach (string s in Names) this[s].Save(path + s + ".py"); }
+
+        public SCRIPTS(string path)
+            : base(path)
+        {
+            Empty = new SCRIPT();
+        }
+
+        public override dynamic Load(string _name)
+        {
+            SCRIPT script = new SCRIPT();
+            try
+            {
+                StreamReader sr;
+                string filename = "";
+                if (!_name.Contains("\\")) filename = path; //default directory
+                filename += _name;
+                if (!_name.Contains(".")) filename += ".py"; //default extension
+                if (File.Exists(filename))
+                {
+                    sr = new StreamReader(filename);
+                    script.name = _name;
+                    script.data = sr.ReadToEnd();
+                    sr.Close();
+                }
+                else
+                {
+                     ConsoleWindow.TraceMessage("Loading Script ERROR: " + filename + " doesn't exist");
+                }
+            }
+            catch (Exception ee)
+            {
+                // insert your pack loading here
+                 ConsoleWindow.TraceMessage("Loading Script ERROR: " + ee.Message);
+            }
+            return script;
+        }
+
+
+    }
+    
+    public class SCRIPT
+    {
+        public string data = "# ", name = "Empty";
+        public int counter = 0;
+        public SCRIPT() { }
+        public void Dispose() { data = ""; name = ""; }
+    }
+
+}
---
+++ ResourceCollectorXNA/ResourceCollectorXNA/UIContent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+using System.IO;
+
+namespace ResourceCollectorXNA
+{
+    
+    public abstract class AutoLoadingContent : Hashtable
+    {
+        public dynamic Empty;
+        public string path = "";
+        public List<string> Names = new List<string>();
+        public AutoLoadingContent(string Path)
+            : base()
+        {
+            path = "";
+            if (!Path.Contains(":")) path = ResourceCollector.AppConfiguration.AppPath + "\\";
+            if (Path.LastIndexOf("\\")>0) path += Path.Substring(0, Path.LastIndexOf("\\")+1);
+
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+        }
+
+        public dynamic GetDirectly(string _name) { dynamic f = this[_name]; return this[_name]; }
+
+        public dynamic this[string key]
+        {
+            get
+            {
+                if (base[key] == null)
+                { Add(key, Load(key)); Names.Add(key); }
+                return base[key];
+            }
+            set
+            {
+                base[key] = value;
+            }
+        }
+
+        public abstract dynamic Load(string _name);
+    }
+
+}
